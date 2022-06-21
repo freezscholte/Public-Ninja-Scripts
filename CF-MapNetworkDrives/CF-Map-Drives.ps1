9e124f633936752e6bbaef4b1e4236fbecd87264
Jan Scholte <52416805+freezscholte@users.noreply.github.com>
2022-06-21 14:02:06 +0200

    Update CF-Map-Drives.ps1

--- CF-MapNetworkDrives/CF-Map-Drives.ps1
+++ CF-MapNetworkDrives/CF-Map-Drives.ps1
@@ -1,4 +1,3 @@
-
 $Drives = Get-ItemProperty "Registry::HKEY_USERS\*\Network\*"
 
 # See if any drives were found
@@ -6,15 +5,10 @@ if ( $Drives ) {
 
    $OutputArray = ForEach ( $Drive in $Drives ) {
 
-        # PSParentPath looks like this: Microsoft.PowerShell.Core\Registry::HKEY_USERS\S-1-5-21-##########-##########-##########-####\Network
         $SID = ($Drive.PSParentPath -split '\\')[2]
-
-           $data = [PSCustomObject]@{
-            # Use .NET to look up the username from the SID
-            Username            = ([System.Security.Principal.SecurityIdentifier]"$SID").Translate([System.Security.Principal.NTAccount])
-            DriveLetter         = $Drive.PSChildName
-            RemotePath          = $Drive.RemotePath
-            SID                 = $SID
+        $Username = ([System.Security.Principal.SecurityIdentifier]"$SID").Translate([System.Security.Principal.NTAccount])
+           $Data = [PSCustomObject]@{
+            Output  = "User: $($Username) - Driveletter: $($Drive.PSChildName) - Path: $($Drive.RemotePath)"
         }
         
         $Data
@@ -28,30 +22,9 @@ if ( $Drives ) {
 
 }
 
-<#
-(Get-CimInstance Win32_OperatingSystem).ProductType
-1 - Work Station
-2 - Domain Controller
-3 - Server
-#>
 
-$MyOS = (Get-CimInstance Win32_OperatingSystem).ProductType
-#If server only list mapped drives not users included
-if ($MyOS -eq 3 ){
-  
-  $Customfield = $OutputArray.RemotePath | Sort-Object -Unique | Format-List | Out-String
-  
-} else {
-  
-  $Customfield = "User: $($OutputArray.Username) - Driveletter: $($OutputArray.DriveLetter) - Path: $($OutputArray.Remotepath)" | Format-List | Out-String
-  
-  
-}
+$Customfield = $OutputArray.Output | Format-List | Out-String
 
-#Write Customfield for Console Ouput
 $Customfield
 
 Ninja-Property-Set networkDrives $Customfield
-
-
-
